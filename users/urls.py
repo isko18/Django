f52a9dcc1db2b76a7c16d6d0ecbcd7a2c18bf7c5
isko18 <toksonbaevislam2004@gmail.com>
2023-04-20 20:59:35 +0600

    hm7

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,37 @@
-urlpatterns = [
+from django.urls import path
+from users.views import RegisterView
 
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
